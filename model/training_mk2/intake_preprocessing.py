cd67fc7c3762331413c79a9a9b113b50f5e84e75
Blair Stevens <stevens.blair.e@gmail.com>
2020-06-02 21:47:03 -0400

    got almost nothing done tonight, but lets check it in anyway

--- model/training_mk2/intake_preprocessing.py
+++ model/training_mk2/intake_preprocessing.py
@@ -4,24 +4,31 @@ import numpy as np
 import pathlib
 import IPython.display as display
 from PIL import Image
+import os
+import matplotlib.pyplot as plt
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 #point to your images
 img_dir = ('model/training_mk2/set_data/')
-
-
 
 data_dir = pathlib.Path(img_dir)
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
 image_count
 
+BATCH_SIZE = 32
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
+
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != ".DS_Store"])
 CLASS_NAMES
 
-ak = list(data_dir.glob('ak 47/*'))
-
-for image_path in ak[:3]:
-    display.display(Image.open(str(image_path)))
+# ak = list(data_dir.glob('ak 47/*'))
+#
+# for image_path in ak[:3]:
+#     display.display(Image.open(str(image_path)))
 
 list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
 
@@ -34,22 +41,67 @@ def get_label(file_path):
   # The second to last is the class-directory
   return parts[-2] == CLASS_NAMES
 
+def decode_img(img):
+  # convert the compressed string to a 3D uint8 tensor
+  img = tf.image.decode_jpeg(img, channels=3)
+  # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+  img = tf.image.convert_image_dtype(img, tf.float32)
+  # resize the image to the desired size.
+  return tf.image.resize(img, [IMG_WIDTH, IMG_HEIGHT])
+
+def process_path(file_path):
+  label = get_label(file_path)
+  # load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+labeled_ds = list_ds.map(process_path,num_parallel_calls=AUTOTUNE)
+
+for image, label in labeled_ds.take(1):
+  print("Image shape: ", image.numpy().shape)
+  print("Label: ", label.numpy())
+
+def prepare_for_training(ds, cache="./set_data.tfcache", shuffle_buffer_size=1000):
+  # This is a small dataset, only load it once, and keep it in memory.
+  # use `.cache(filename)` to cache preprocessing work for datasets that don't
+  # fit in memory.
+  if cache:
+    if isinstance(cache, str):
+      ds = ds.cache(cache)
+    else:
+      ds = ds.cache()
+
+  ds = ds.shuffle(buffer_size=shuffle_buffer_size)
+
+  # Repeat forever
+  ds = ds.repeat()
+
+  ds = ds.batch(BATCH_SIZE)
+
+  # `prefetch` lets the dataset fetch batches in the background while the model
+  # is training.
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
 
 
+  return ds
 
 
+train_ds = prepare_for_training(labeled_ds)
 
+train_ds
 
+image_batch, label_batch = next(iter(train_ds))
 
+def show_batch(image_batch, label_batch):
+  plt.figure(figsize=(10,10))
+  for n in range(25):
+      ax = plt.subplot(5,5,n+1)
+      plt.imshow(image_batch[n])
+      plt.title(CLASS_NAMES[label_batch[n]==1][0].title())
+      plt.axis('off')
 
+show_batch(image_batch.numpy(), label_batch.numpy())
 
-def get_images_paths(img_path):
-    list_img = tf.data.Dataset.list_files(img_path)
-    paths = next(iter(list_img))
-    return paths
-
-def parse_images(paths):
-    parts = tf.string.split(paths, '/')
-    label = parts[-2]
-
-    image = tf.io.read_file(paths)
+print(train_ds)
